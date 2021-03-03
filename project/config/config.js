1af6eff9537a70c002b8c8e79aa9c74a238540b8
ravimoryaAu13e <ravi.morya2020@gmail.coml>
2021-03-03 10:50:37 +0530

    main page done

--- project/config/config.js
+++
@@ -1,6 +0,0 @@
-// one string over which token will generate
-module.exports = {
-    'secret':'ravisecret'
-}
-
-/// userid+secret = token
--- project/config/db.js
+++
@@ -1,4 +0,0 @@
-// connected with DB
-var mongoose = require('mongoose');
-// cvraman is db name
-mongoose.connect('mongodb://localhost:27017/ravi')
